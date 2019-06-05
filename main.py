3cffda14a7485fdfeb3341fd5b5c24e08eb0ee16
Zachery Thomas (zacthoma) <zacthoma@cisco.com>
2019-06-04 22:19:48 -0400

    Use stream listner instead of mentions_api

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 __author__ = 'Zachery Thomas'
 
 import time
+import re
+import threading
 
 import docker
 import tweepy
@@ -8,7 +10,9 @@ import tweepy
 from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET
 
 CLIENT = docker.from_env()
-SLEEP_TIMER = 10
+AUTH = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
+AUTH.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+API = tweepy.API(auth_handler=AUTH, retry_count=3)
 
 def fetch_mentions(api):
     """Fetch mentions from twitter"""
@@ -32,11 +36,7 @@ def clean_tweet_text(tweet):
     """Removes user mentions from tweet"""
     tweet_text = tweet.text
 
-    for user_mentioned in tweet.entities['user_mentions']:
-        screen_name = user_mentioned['screen_name']
-        tweet_text = tweet_text.replace(('@{}').format(screen_name), '')
-
-    return tweet_text.strip()
+    return re.sub(r'@\S*', '', tweet_text).strip()
 
 
 def start_container(name):
@@ -59,49 +59,65 @@ def run_command(container, cmd):
 
 
 def response_formatter(text, username, max_length=140):
-    """Formats response to keep tweet under 140 chars"""
     while len('{} @{}'.format(text, username)) > max_length:
         text = text[:-1]
 
     return '{} @{}'.format(text, username)
 
 
+class WorkerThread (threading.Thread):
+    def __init__(self, tweet):
+        threading.Thread.__init__(self)
+        self.tweet = tweet
+
+    def run(self):
+        if self.tweet.favorited:
+            return
+
+        print('tweet not fav, continue')
+        fav_tweet(API, self.tweet)
+
+        user_id = str(self.tweet.user.id)
+        username = str(self.tweet.user.screen_name)
+        tweet_text = clean_tweet_text(self.tweet)
+
+        print('text: {}'.format(tweet_text))
+        print('container name: {}'.format(user_id))
+        try:
+            container = CLIENT.containers.get(user_id)
+
+        except docker.errors.NotFound:
+            print('Container not found for {}, starting one...'.format(username))
+            container = start_container(username)
+            print('Started container for {} as {}'.format(username, user_id))
+
+        res_text, res_code = run_command(container, tweet_text)
+        res_text = response_formatter(res_text, username, max_length=140)
+        print(res_text, res_code)
+
+        API.update_status(res_text, in_reply_to_status_id=self.tweet.id)
+
+
+class StreamHelper (tweepy.streaming.StreamListener):
+    def on_status(self, status):
+        wt = WorkerThread(status)
+        wt.start()
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            print('Hit rate limit :(')
+
+            #returning False in on_data disconnects the stream
+            return False
+
+
 def main():
     """Main function"""
-    auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    sh = StreamHelper()
+    stream = tweepy.Stream(AUTH, sh, timeout=None)
 
-    api = tweepy.API(auth_handler=auth, retry_count=3)
-
-    while 1:
-        results = fetch_mentions(api)
-        for tweet in results:
-            if tweet.favorited:
-                continue
-            fav_tweet(api, tweet)
-
-
-            username = tweet.user.screen_name
-            tweet_text = clean_tweet_text(tweet)
-
-            print('text: {}'.format(tweet_text))
-            print('container username: {}'.format(username))
-            try:
-                container = CLIENT.containers.get(username)
-
-            except docker.errors.NotFound:
-                print('Container not found, starting one...')
-                container = start_container(username)
-                print('Done!')
-
-            res_text, res_code = run_command(container, tweet_text)
-            res_text = response_formatter(res_text, username, max_length=140)
-            print(res_text, res_code)
-
-            api.update_status(res_text, in_reply_to_status_id=tweet.id)
-
-
-        time.sleep(SLEEP_TIMER)
+    screen_name = API.me().screen_name
+    stream.filter(track=[screen_name])
 
 
 if __name__ == "__main__":
