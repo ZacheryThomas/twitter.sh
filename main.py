e1bba25bb9556aac662d284afdf7c6961d2859f0
Zachery Thomas (zacthoma) <zacthoma@cisco.com>
2019-06-04 22:06:25 -0400

    Update main code

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 __author__ = 'Zachery Thomas'
 
+import time
+
 import docker
 import tweepy
 
 from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET
 
 CLIENT = docker.from_env()
-
+SLEEP_TIMER = 10
 
 def fetch_mentions(api):
     """Fetch mentions from twitter"""
     return api.mentions_timeline()
+
+
+def fav_tweet(api, reply):
+    """Attempt to fav a tweet and return True if successful"""
+
+    # sometimes this raises TweepError even if reply.favorited
+    # was False
+    try:
+        api.create_favorite(id=reply.id)
+    except tweepy.TweepError:
+        return False
+
+    return True
 
 
 def clean_tweet_text(tweet):
@@ -33,7 +48,7 @@ def start_container(name):
 
 def run_command(container, cmd):
     """Runs command given container obj and cmd string"""
-    cmd = str(cmd)
+    cmd = 'sh -c "{}"'.format(str(cmd))
 
     try:
         res = container.exec_run(cmd)
@@ -43,6 +58,14 @@ def run_command(container, cmd):
         return str(exc), 1
 
 
+def response_formatter(text, username, max_length=140):
+    """Formats response to keep tweet under 140 chars"""
+    while len('{} @{}'.format(text, username)) > max_length:
+        text = text[:-1]
+
+    return '{} @{}'.format(text, username)
+
+
 def main():
     """Main function"""
     auth = tweepy.OAuthHandler(consumer_key=CONSUMER_KEY, consumer_secret=CONSUMER_SECRET)
@@ -50,26 +73,35 @@ def main():
 
     api = tweepy.API(auth_handler=auth, retry_count=3)
 
-    results = fetch_mentions(api)
+    while 1:
+        results = fetch_mentions(api)
+        for tweet in results:
+            if tweet.favorited:
+                continue
+            fav_tweet(api, tweet)
 
-    for tweet in results:
-        username = tweet.user.screen_name
-        tweet_text = clean_tweet_text(tweet)
 
-        print('text: {}'.format(tweet_text))
-        print('container username: {}'.format(username))
-        try:
-            container = CLIENT.containers.get(username)
+            username = tweet.user.screen_name
+            tweet_text = clean_tweet_text(tweet)
 
-        except docker.errors.NotFound:
-            print('Container not found, starting one...')
-            container = start_container(username)
-            print('Done!')
+            print('text: {}'.format(tweet_text))
+            print('container username: {}'.format(username))
+            try:
+                container = CLIENT.containers.get(username)
 
-        res_text, res_code = run_command(container, tweet_text)
-        print(res_text, res_code)
+            except docker.errors.NotFound:
+                print('Container not found, starting one...')
+                container = start_container(username)
+                print('Done!')
 
-        api.update_status(res_text, in_reply_to_status_id=tweet.id)
+            res_text, res_code = run_command(container, tweet_text)
+            res_text = response_formatter(res_text, username, max_length=140)
+            print(res_text, res_code)
+
+            api.update_status(res_text, in_reply_to_status_id=tweet.id)
+
+
+        time.sleep(SLEEP_TIMER)
 
 
 if __name__ == "__main__":
